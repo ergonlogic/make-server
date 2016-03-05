50a12095bf38faa9d5331d71ae9b6e33635d1c65
Christopher Gervais <chris@ergonlogic.com>
2016-03-04 20:51:55 -0500

    Fix env var checks.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -2,15 +2,14 @@
 
 export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=true
-truthy="true 1"
 
-if [[ $truthy =~ $VAGRANT ]]; then
+if [[ $VAGRANT == 'true' ]]; then
   ANSIBLE_DIR=/vagrant/ansible
 else
   ANSIBLE_DIR=./ansible
 fi
 
-if [[ $truthy =~ $DRUSH_DEV ]]; then
+if [[ $DRUSH_DEV == 'true' ]]; then
   curl -sSL https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/bash
   ansible-galaxy install -r $ANSIBLE_DIR/requirements.yml -p $ANSIBLE_DIR/roles/ --ignore-errors
   ansible-playbook $ANSIBLE_DIR/drush_dev.yml -i $ANSIBLE_DIR/inventory
