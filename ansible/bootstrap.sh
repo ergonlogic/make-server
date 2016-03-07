fe24fd487f9d9b63a7234ffeddb9a53b07b53943
Christopher Gervais <chris@ergonlogic.com>
2016-03-07 01:56:49 -0500

    Add Behat.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -3,21 +3,22 @@
 export PYTHONUNBUFFERED=1
 export ANSIBLE_FORCE_COLOR=true
 
-# Isolate differences between Vagrant and TravisCI environments.
+# Set different project roots depending on Vagrant or TravisCI environments.
 if [[ $2 == 'true' ]]; then
-  ANSIBLE_DIR=/vagrant/ansible
   ROOT=/vagrant
+  VAGRANT=true
 else
-  ANSIBLE_DIR=./ansible
   ROOT=$TRAVIS_BUILD_DIR
+  VAGRANT=false
 fi
+ANSIBLE_DIR=$ROOT/ansible
 
 # Default to packaged versions of Ansible and Drush, but allow installation
 # from source.
 if [[ $1 == 'true' ]]; then
   curl -sSL https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/bash
   ansible-galaxy install -r $ANSIBLE_DIR/requirements.yml -p $ANSIBLE_DIR/roles/ --ignore-errors
-  ansible-playbook $ANSIBLE_DIR/plays/drush_dev.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT"
+  ansible-playbook $ANSIBLE_DIR/plays/drush_dev.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
 else
   echo "Installing Ansible"
   apt-get -y install software-properties-common > /dev/null
@@ -25,9 +26,11 @@ else
   apt-get update -qq > /dev/null
   apt-get -y install -o Dpkg::Options::="--force-confnew" ansible > /dev/null
   echo "Installing Drush"
-  ansible-playbook $ANSIBLE_DIR/plays/drush.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT"
+  ansible-playbook $ANSIBLE_DIR/plays/drush.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
 fi
 
 echo "Installing Drupal"
-ansible-playbook $ANSIBLE_DIR/plays/drupal.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT"
+ansible-playbook $ANSIBLE_DIR/plays/drupal.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
 
+echo "Installing Behat"
+ansible-playbook $ANSIBLE_DIR/plays/behat.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
