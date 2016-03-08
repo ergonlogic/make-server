9f8820d6ac3e68a7252184464b165c038a5f7161
Christopher Gervais <chris@ergonlogic.com>
2016-03-08 09:16:22 -0500

    Ansible 2.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -10,7 +10,7 @@ if [[ $1 == 'true' ]]; then
   if [[ $install_ansible == 1 ]]; then
     echo "Installing Ansible"
     sudo apt-get -y install software-properties-common
-    sudo apt-add-repository -y ppa:ansible/ansible-1.9
+    sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt-get update -qq
     sudo apt-get -y install -o Dpkg::Options::="--force-confnew" ansible
   fi
@@ -19,6 +19,7 @@ else
   SCRIPT_USER=travis
   echo "Installing Ansible"
   if [[ $install_ansible == 1 ]]; then
+    pip install --upgrade pip
     pip install ansible
   fi
 fi
@@ -28,11 +29,11 @@ export ANSIBLE_FORCE_COLOR=true
 PLAYBOOK_DIR="$ROOT/ansible/plays"
 
 echo "Installing Drush"
-ansible-playbook $PLAYBOOK_DIR/drush.yml
+ansible-playbook $PLAYBOOK_DIR/drush.yml -i $ROOT/ansible/hosts
 
 echo "Installing Drupal"
-ansible-playbook $PLAYBOOK_DIR/drupal.yml --extra-vars="root=$ROOT"
+ansible-playbook $PLAYBOOK_DIR/drupal.yml -i $ROOT/ansible/hosts -e "root=$ROOT"
 
 echo "Installing Behat"
-ansible-playbook $PLAYBOOK_DIR/behat.yml -e "root=$ROOT"
+ansible-playbook $PLAYBOOK_DIR/behat.yml -i $ROOT/ansible/hosts -e "root=$ROOT"
 
