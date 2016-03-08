07f660ecbe2922a6c41b663162fa1da48db956b9
Christopher Gervais <chris@ergonlogic.com>
2016-03-08 10:43:51 -0500

    Just use apt.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -2,25 +2,21 @@
 
 which ansible > /dev/null
 install_ansible=$?
+if [[ $install_ansible == 1 ]]; then
+  echo "Installing Ansible"
+  sudo apt-get -y install software-properties-common
+  sudo apt-add-repository -y ppa:ansible/ansible
+  sudo apt-get update -qq
+  sudo apt-get -y install -o Dpkg::Options::="--force-confnew" ansible
+fi
 
 # Set different project roots depending on Vagrant or TravisCI environments.
 if [[ $1 == 'true' ]]; then
   ROOT=/vagrant
   SCRIPT_USER=vagrant
-  if [[ $install_ansible == 1 ]]; then
-    echo "Installing Ansible"
-    sudo apt-get -y install software-properties-common
-    sudo apt-add-repository -y ppa:ansible/ansible
-    sudo apt-get update -qq
-    sudo apt-get -y install -o Dpkg::Options::="--force-confnew" ansible
-  fi
 else
   ROOT=$TRAVIS_BUILD_DIR
   SCRIPT_USER=travis
-  echo "Installing Ansible"
-  if [[ $install_ansible == 1 ]]; then
-    pip install ansible
-  fi
 fi
 
 export PYTHONUNBUFFERED=1
