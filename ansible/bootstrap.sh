27d4dd5cb2a89497c07dc107a52b5b84894a0756
Christopher Gervais <chris@ergonlogic.com>
2016-03-08 07:27:23 -0500

    Simplify everything.

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-export PYTHONUNBUFFERED=1
-export ANSIBLE_FORCE_COLOR=true
+which ansible > /dev/null
+install_ansible=$?
 
 # Set different project roots depending on Vagrant or TravisCI environments.
-if [[ $2 == 'true' ]]; then
+if [[ $1 == 'true' ]]; then
   ROOT=/vagrant
-  VAGRANT=true
+  SCRIPT_USER=vagrant
+  if [[ $install_ansible == 1 ]]; then
+    echo "Installing Ansible"
+    sudo apt-get -y install software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible-1.9
+    sudo apt-get update -qq
+    sudo apt-get -y install -o Dpkg::Options::="--force-confnew" ansible
+  fi
 else
   ROOT=$TRAVIS_BUILD_DIR
-  VAGRANT=false
-fi
-ANSIBLE_DIR=$ROOT/ansible
-
-# Default to packaged versions of Ansible and Drush, but allow installation
-# from source.
-if [[ $1 == 'true' ]]; then
-  curl -sSL https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/bash
-  ansible-galaxy install -r $ANSIBLE_DIR/requirements.yml -p $ANSIBLE_DIR/roles/ --ignore-errors
-  ansible-playbook $ANSIBLE_DIR/plays/drush_dev.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
-else
+  SCRIPT_USER=travis
   echo "Installing Ansible"
-  apt-get -y install software-properties-common > /dev/null
-  apt-add-repository -y ppa:ansible/ansible-1.9 &> /dev/null
-  apt-get update -qq > /dev/null
-  apt-get -y install -o Dpkg::Options::="--force-confnew" ansible > /dev/null
-  echo "Installing Drush"
-  ansible-playbook $ANSIBLE_DIR/plays/drush.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
+  if [[ $install_ansible == 1 ]]; then
+    pip install ansible
+  fi
 fi
+
+export PYTHONUNBUFFERED=1
+export ANSIBLE_FORCE_COLOR=true
+PLAYBOOK_DIR="$ROOT/ansible/plays"
+
+echo "Installing Drush"
+ansible-playbook $PLAYBOOK_DIR/drush.yml
 
 echo "Installing Drupal"
-ansible-playbook $ANSIBLE_DIR/plays/drupal.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
+ansible-playbook $PLAYBOOK_DIR/drupal.yml --extra-vars="root=$ROOT"
 
 echo "Installing Behat"
-ansible-playbook $ANSIBLE_DIR/plays/behat.yml -i $ANSIBLE_DIR/hosts -e "root=$ROOT" -e "vagrant=$VAGRANT"
+ansible-playbook $PLAYBOOK_DIR/behat.yml -e "root=$ROOT"
+
