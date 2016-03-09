b0937c9b3ebc38af53d6ceafdc20cefdca63f07a
Christopher Gervais <chris@ergonlogic.com>
2016-03-09 11:17:03 -0500

    Fix Behat.

--- .mk/behat.mk
+++ .mk/behat.mk
@@ -15,7 +15,7 @@ deps-selenium: vagrant aptitude-update
 selenium: vm
 	@curl -sSL -z $(selenium) -o $(selenium) http://selenium-release.storage.googleapis.com/$(selenium-version)/selenium-server-standalone-$(selenium-release).jar
 
-drush-bde-env: vm init $(bin_dir)/drush drupal
+drush-bde-env: vm $(bin_dir)/drush $(root)
 	@git clone https://github.com/pfrenssen/drush-bde-env.git ~/.drush/drush-bde-env
 	@$(drush) cc drush
 	@cd $(project_root) && $(drush) beg --subcontexts=profiles/$(profile)/modules --site-root=$(root) --skip-path-check --base-url=$(uri) $(project_root)/behat_params.sh
@@ -24,7 +24,7 @@ deps-behat: vagrant aptitude-update composer
 	@echo Installing Behat dependencies
 	@sudo DEBIAN_FRONTEND=noninteractive apt-get -y -qq install php5-curl
 
-behat: vm
+behat: vm drush-bde-env
 ifeq '$(project_root)' '/vagrant'
 	cd $(project_root) && $(composer) install
 else
